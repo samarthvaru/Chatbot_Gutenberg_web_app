06bd57e28b8eabfae3ed4027263b7237d92eb293
Samarth Varu <samarthvaru111@gmail.com>
2024-02-01 03:08:38 -0500

    changed chatbot functionality from func to class

--- app.py
+++ app.py
@@ -41,160 +41,90 @@ class ChatHistory:
         self.user_turn = user_turn
         self.chatbot_reply = chatbot_reply
 
-# A simple tokenizer. Applies case folding
-def tokenize(s):
-    tokens = s.lower().split()
-    trimmed_tokens = []
-    for t in tokens:
-        if re.search('\w', t):
-            # t contains at least 1 alphanumeric character
-            t = re.sub('^\W*', '', t)  # trim leading non-alphanumeric chars
-            t = re.sub('\W*$', '', t)  # trim trailing non-alphanumeric chars
-        trimmed_tokens.append(t)
-    return trimmed_tokens
+class VectorSimilarityCalculator:
+    def __init__(self, responses_fname, vectors_fname):
+        self.responses = [x.strip() for x in open(responses_fname)]
+        self.word_vectors = self.load_vectors(vectors_fname)
+        self.calc_query_vector = {}
+        self.resp_vec = {}
 
-# Find the most similar response in terms of token overlap.
-def most_sim_overlap(query, responses):
-    q_tokenized = tokenize(query)
-    max_sim = 0
-    max_resp = "Sorry, I don't understand"
-    for r in responses:
-        r_tokenized = tokenize(r)
-        sim = len(set(r_tokenized).intersection(q_tokenized))
-        if sim > max_sim:
-            max_sim = sim
-            max_resp = r
-    return max_resp
+    def tokenize(self, s):
+        tokens = s.lower().split()
+        return [re.sub('\W*', '', t) for t in tokens if re.search('\w', t)]
 
-# Code for loading the fasttext (word2vec) vectors
-# from here (lightly modified):
-# https://fasttext.cc/docs/en/crawl-vectors.html
-def load_vectors(fname):
-    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-    data = {}
-    for line in fin:
-        tokens = line.rstrip().split(' ')
-        data[tokens[0]] = list(map(float, tokens[1:]))
-    return data
+    def load_vectors(self, fname):
+        with io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore') as fin:
+            data = {tokens[0]: list(map(float, tokens[1:])) for line in fin for tokens in [line.rstrip().split(' ')]}
+        return data
 
-def sum_lists(*args):
-    return list(map(sum, zip(*args)))
+    def normalize_vector(self, vector):
+        norm_term = sum(v ** 2 for v in vector)
+        norm_term = math.sqrt(norm_term)
+        return [v / norm_term for v in vector]
 
-def response_vector_calc(word_vec, responses):
-    t = {}
-    i = 0
-    result = [0] * 300
-    for res in responses:
-        r_tokenize = tokenize(res)
-        norm_vec = {}
-        n = 0
-        for word in r_tokenize:
-            emb = {}
-            if word in word_vec.keys():
-                emb[word] = word_vec[word]
-                norm_den = {}
-                norm_term = 0
-                for v in emb[word]:
-                    norm_term += v ** 2
-                norm_term = math.sqrt(norm_term)
-                norm_den[word] = norm_term
+    def calculate_response_vector(self, responses):
+        result = [0] * 300
+        for res in responses:
+            r_tokenize = self.tokenize(res)
+            norm_vec = {word: self.normalize_vector(self.word_vectors[word]) for word in r_tokenize if word in self.word_vectors}
+            n = len(norm_vec) if norm_vec else len(r_tokenize)
+            l = [vec for vec in norm_vec.values()]
 
-                s = []
-                for v in emb[word]:
-                    s.append(v / norm_den[word])
+            for i in range(len(l)):
+                result = [x + y for x, y in zip(result, l[i])]
 
-                norm_vec[word] = s
-                n = n + 1
+            result = [x / n for x in result]
+            self.resp_vec[res] = result
 
-        if n == 0:
-            n = len(r_tokenize)
-        l = []
-        for i in norm_vec.values():
-            l.append(i)
+    def calculate_query_vector(self, query):
+        result = [0] * 300
+        q_tokenize = self.tokenize(query)
+        norm_vec = {word: self.normalize_vector(self.word_vectors[word]) for word in q_tokenize if word in self.word_vectors}
+        n = len(norm_vec) if norm_vec else len(q_tokenize)
+        l = [vec for vec in norm_vec.values()]
 
         for i in range(len(l)):
-            a = result
-            b = l[i]
-            result = sum_lists(a, b)
-        result[:] = [x / n for x in result]
-        t[res] = result
+            result = [x + y for x, y in zip(result, l[i])]
 
-    return t
+        result = [x / n for x in result]
+        self.calc_query_vector[query] = result
 
-def vec_calc(word_vec, query):
-    t = {}
-    result = [0] * 300
-    q_tokenize = tokenize(query)
-    norm_vec = {}
-    n = 0
-    for word in q_tokenize:
-        emb = {}
+    def most_sim_overlap(self, query):
+        q_tokenized = self.tokenize(query)
+        max_sim = 0
+        max_resp = "Sorry, I don't understand"
+        for r in self.responses:
+            r_tokenized = self.tokenize(r)
+            sim = len(set(r_tokenized).intersection(q_tokenized))
+            if sim > max_sim:
+                max_sim = sim
+                max_resp = r
+        return max_resp
 
-        if word in word_vec.keys():
-            emb[word] = word_vec[word]
-            norm_den = {}
-            norm_term = 0
-            for v in emb[word]:
-                norm_term += v ** 2
-            norm_term = math.sqrt(norm_term)
-            norm_den[word] = norm_term
+    def calculate_similarity(self, query):
+        max_sim = 0
+        max_resp = "Sorry, I don't understand"
+        query_vector = self.calc_query_vector[query]
+        query_mag = math.sqrt(sum(v ** 2 for v in query_vector))
 
-            s = []
-            for v in emb[word]:
-                s.append(v / norm_den[word])
+        for res in self.responses:
+            resp_vector = self.resp_vec[res]
+            resp_mag = math.sqrt(sum(v ** 2 for v in resp_vector))
+            tot_mag = query_mag * resp_mag
 
-            norm_vec[word] = s
-            n = n + 1
+            if tot_mag == 0:
+                return "Sorry, I don't understand"
 
-    if n == 0:
-        n = len(q_tokenize)
-    l = []
-    for i in norm_vec.values():
-        l.append(i)
+            vec_mul = [x * y for x, y in zip(query_vector, resp_vector)]
+            cosine_sim = sum(vec_mul) / tot_mag
 
-    for i in range(len(l)):
-        a = result
-        b = l[i]
-        result = sum_lists(a, b)
-    result[:] = [x / n for x in result]
-    t[query] = result
-    return t
+            if cosine_sim > max_sim:
+                max_sim = cosine_sim
+                max_resp = res
 
-def most_sim(calc_query_vector, resp_vec, responses, query):
-    max_sim = 0
-    max_resp = "Sorry, I don't understand"
-    norm_term = 0
-
-    for v in calc_query_vector[query]:
-        norm_term += v ** 2
-    norm_term = math.sqrt(norm_term)
-    query_mag = norm_term
-
-    for res in responses:
-        cosine_sim = 0
-        norm_term = 0
-
-        for v in resp_vec[res]:
-            norm_term += v ** 2
-        norm_term = math.sqrt(norm_term)
-        resp_mag = norm_term
-        tot_mag = query_mag * resp_mag
-
-        vec_mul = list(map(lambda x, y: x * y, calc_query_vector[query], resp_vec[res]))
-        summation = 0
-        for i in vec_mul:
-            summation += i
-        if tot_mag == 0:
-            return "Sorry, I don't understand"
-
-        cosine_sim = summation / tot_mag
-
-        if cosine_sim > max_sim:
-            max_sim = cosine_sim
-            max_resp = res
-
-    return max_resp
-
+        return max_resp
+    
+    
 @app.route('/')
 def home():
     return render_template('homepage.html')
@@ -214,11 +144,12 @@ def chat():
     if end_chat:
         return redirect(url_for('index'))
 
+
     if method == 'overlap':
-        response = most_sim_overlap(user_turn, responses)
+        response = vector_similarity_calculator.most_sim_overlap(user_turn)
     elif method == 'w2v':
-        calc_query_vector = vec_calc(word_vectors, user_turn)
-        response = most_sim(calc_query_vector, resp_vec, responses, user_turn)
+        vector_similarity_calculator.calculate_query_vector(user_turn)
+        response = vector_similarity_calculator.calculate_similarity(user_turn)
 
     # Add the current turn to the conversation
     insert_db(
@@ -273,13 +204,20 @@ def delete_chat():
 def delete_entire_chat():
     user_email = request.args.get('email', '')
 
-    # Delete the entire chat history for the user from the database
-    conn = get_db()
-    conn.execute('DELETE FROM chat_history WHERE email = ?', (user_email,))
-    conn.commit()
+    try:
+        # Delete the entire chat history for the user from the database
+        conn = get_db()
+        conn.execute('DELETE FROM chat_history WHERE email = ?', (user_email,))
+        conn.commit()
 
-    # Return a JSON response indicating success
-    return jsonify(success=True)
+        # Return a JSON response indicating success
+        return jsonify(success=True)
+    
+    except Exception as e:
+        # Print the error message for debugging
+        print(f"Error deleting chat: {str(e)}")
+
+        return jsonify(success=False, error=str(e))    
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
@@ -300,11 +238,11 @@ def clear_chat_history(user_email):
 if __name__ == '__main__':
     # Load vectors and initialize chatbot
     vectors_fname = 'cc.en.300.vec.10k'
-    word_vectors = load_vectors(vectors_fname)
     responses_fname = 'gutenberg.txt'
-    responses = [x.strip() for x in open(responses_fname)]
-    resp_vec = response_vector_calc(word_vectors, responses)
+    vector_similarity_calculator = VectorSimilarityCalculator(responses_fname,vectors_fname)
+    vector_similarity_calculator.calculate_response_vector(vector_similarity_calculator.responses)
 
+    
     app.run(host='0.0.0.0',port=5000)
 
 
